1c8d86956a3f3b81ac191ae2d465d3d95d86ae55
Ahmad <ahmadmsalman551@gmail.com>
2026-03-12 01:44:47 +0300

    just fake push to origin development

---
+++ lib/features/onboarding/widgets/get_started_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tasks/core/helpers/extentions.dart';
+import 'package:tasks/core/routing/routes.dart';
+import 'package:tasks/core/theming/app_colors.dart';
+import 'package:tasks/core/theming/styles.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(AppColors.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: WidgetStateProperty.all(Size(1.sw, 52.h)),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+        ),
+      ),
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      child: Text("Get Started", style: AppTextStyles.font16whitesemibold),
+    );
+  }
+}
