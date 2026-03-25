a0aaf46eae018ae39f17237ca39e2bf321ee0a9c
Ahmad <ahmadmsalman551@gmail.com>
2026-03-25 22:56:03 +0300

    added splash screen for android under 12  and upper 12

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:tasks/app.dart';
 import 'package:tasks/core/routing/app_route.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.remove(); // 🔥 هذا المهم
+
   runApp(MyApp(appRoute: AppRoute()));
 }
